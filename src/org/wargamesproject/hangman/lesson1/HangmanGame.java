3b4a83f859713a6514db38eae34b5b6c2aa187fd
mpickerng1 <mpickering@mindspring.com>
2017-05-21 18:05:43 -0400

    Lesson 1 of Hangman Game

---
+++ src/org/wargamesproject/hangman/lesson1/HangmanGame.java
@@ -0,0 +1,58 @@
+package org.wargamesproject.hangman.lesson1;
+
+import java.util.Scanner;
+
+public class HangmanGame 
+{
+	// Member variables
+	private Player guessingPlayer,choosingPlayer;
+	// Constants
+	private static final Player COMPUTER_PLAYER = new Player("Computer"); 
+	
+	public HangmanGame(Player guessingPlayer)
+	{
+		this.guessingPlayer = guessingPlayer;
+		this.choosingPlayer = COMPUTER_PLAYER;
+	}
+	
+	public void playGame()
+	{
+		System.out.println("Game play goes here.");
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Hangman!");
+		
+		// Prompt for the player's name.  Make sure they enter at least two characters
+		boolean goodName = false;
+		String playerName = null;
+		
+		Scanner nameScan = new Scanner(System.in);
+		
+		while (goodName == false)
+		{
+			System.out.print("Enter player name: ");
+			playerName = nameScan.nextLine();
+			
+			if (playerName.length() >= 2)
+			{
+				goodName = true;
+				nameScan.close();
+			}
+			else
+			{
+				System.out.println("ERROR: Player name must be at least 2 characters!  Try again...");
+			}
+		}
+		
+		// Create the Player object instance passing in the player's name
+		Player humanPlayer = new Player(playerName);
+		
+		// Create the instance of the HangmanGame object, passing the Player in the constructor
+		HangmanGame hangman = new HangmanGame(humanPlayer);
+		
+		// Play a new game of Hangman by calling playGame() on the new game instance
+		hangman.playGame();
+	}
+}
---
+++ src/org/wargamesproject/hangman/lesson1/Player.java
@@ -0,0 +1,17 @@
+package org.wargamesproject.hangman.lesson1;
+
+public class Player 
+{
+	// Member variables
+	private String name;
+	
+	public Player(String playerName)
+	{
+		this.name = playerName;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+}
